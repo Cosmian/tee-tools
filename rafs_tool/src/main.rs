32addcb788c2a8440bccd00e2633f6dfbf98e47c
Sébastien Lambert <sebastien.lambert@cosmian.com>
2023-09-06 13:53:30 +0000

    :sparkles: add rafs-tool

---
+++ rafs_tool/src/main.rs
@@ -0,0 +1,42 @@
+use anyhow::Result;
+
+use clap::{Parser, Subcommand};
+use decrypt::DecryptArgs;
+use encrypt::EncryptArgs;
+use init::InitArgs;
+use proxy::ProxyArgs;
+
+pub mod common;
+pub mod decrypt;
+pub mod encrypt;
+pub mod init;
+pub mod proxy;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: CliCommands,
+}
+
+#[derive(Subcommand)]
+enum CliCommands {
+    Init(InitArgs),
+    Proxy(ProxyArgs),
+    Encrypt(EncryptArgs),
+    Decrypt(DecryptArgs),
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let opts = Cli::parse();
+
+    match opts.command {
+        CliCommands::Init(args) => args.run(),
+        CliCommands::Proxy(args) => args.run(),
+        CliCommands::Encrypt(args) => args.run().await,
+        CliCommands::Decrypt(args) => args.run(),
+    }?;
+
+    Ok(())
+}
